f1710ad6edc255ed78fe0c86d8b3255f16db35e3
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-05-23 07:39:52 +0530

    added search_insert_positon.py

---
+++ python/search_insert_position.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    # used Binary Search to return the index of the element
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return left
